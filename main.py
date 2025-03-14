7bee10cf3e3db51c0b7abb7fa2c2a79346492b58
Qayleef <105773899+Qayleef@users.noreply.github.com>
2025-03-14 09:21:01 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(
+    page_title="Day2Page",
+    page_icon= ":shark:",
+    layout = "wide"
+)
+
+st.markdown('# This is the main page...')
+st.sidebar.write('Home')
+
+
+
+im = Image.open('Dell_logo.svg.png')
+st.image(im, width=300)
---
+++ pages/page1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown('# this is page 1')
+st.write('this is page 1')
